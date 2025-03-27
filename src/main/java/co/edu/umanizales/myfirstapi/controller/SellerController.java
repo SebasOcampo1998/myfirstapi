dad30ecdcda4c44fc9bdce1a7a86a8801d34261e
DAYANA <sebasocampo1122@gmail.com>
2025-03-26 20:31:16 -0500

    Actualizado

--- src/main/java/co/edu/umanizales/myfirstapi/controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstapi/controller/SellerController.java
@@ -11,17 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class SellerController {
 
     @GetMapping
-    public String getSeller() {
-        Seller sebas = new Seller("67001", "Sebastian", "Ocampo", 'M', (byte) 26,"Manizales");
-        Seller dayana = new Seller("67002", "Dayana", "Rendon", 'F', (byte) 22,"Manizales");
-        Seller orfa = new Seller("67003", "Orfa","Sanchez", 'F', (byte) 56, "Manizales");
-        Seller angely = new Seller("67004", "Angely", "Ospina", 'F', (byte) 33,"Manizales");
-        Seller ferney = new Seller("67005","Ferney","Ocampo", 'M', (byte) 42,"Manizales");
+    public Seller getSeller() {
+        Location manizales = new Location("17001","Manizales");
+        Location pereira = new Location("66001","Pereira");
+
+        Seller sebas = new Seller("67001", "Sebastian", "Ocampo", 'M', (byte) 26,manizales);
+        Seller dayana = new Seller("67002", "Dayana", "Rendon", 'F', (byte) 22,pereira);
+        Seller orfa = new Seller("67003", "Orfa","Sanchez", 'F', (byte) 56, pereira);
+        Seller angely = new Seller("67004", "Angely", "Ospina", 'F', (byte) 33, pereira);
+        Seller ferney = new Seller("67005","Ferney","Ocampo", 'M', (byte) 42,manizales);
 
 
 // En char se debe utilizar 'M' y no se utiliza "M"
 
 
-        return "Clientes";
+        return sebas;
     }
 }
--- src/main/java/co/edu/umanizales/myfirstapi/model/Seller.java
+++ src/main/java/co/edu/umanizales/myfirstapi/model/Seller.java
@@ -14,6 +14,6 @@ public class Seller {
     private String lastName;
     private char gender;
     private byte age;
-    private String city;
+    private Location city;
 
 }
